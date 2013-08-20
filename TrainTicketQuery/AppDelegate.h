6e9d2a6033063c5d1b50c23b2ec06affff795115
_Old_Horse_ <hongliang.ma@3hdtv.com>
2013-08-20 12:47:59 +0800

    Initial Commit

---
+++ TrainTicketQuery/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TrainTicketQuery
+//
+//  Created by M J on 13-8-20.
+//  Copyright (c) 2013年 M J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TrainTicketQuery/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TrainTicketQuery
+//
+//  Created by M J on 13-8-20.
+//  Copyright (c) 2013年 M J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
